278eb7bf12001c69c25f6653f58267ef6749f331
lusta <75475914+AnastasiaLusta@users.noreply.github.com>
2023-01-28 07:03:33 +0000

    feat: add University and Mother class

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ class Student:
 		self.name = name
 		self.gladness = 0
 		self.progress = 0
+		self.money = 0
 		self.alive = True
+	def education(self, University):
+		print(self.name, 'is studying in', University.title)
+	def work(self):
+		self.money += 50
+		self.progress -= 5
+		print('Work time')
 	def study(self):
 		self.progress += 20
 		self.gladness -= 10
@@ -28,6 +35,8 @@ class Student:
 			self.sleep()
 		elif live_cube == 4:
 			self.say_hello()
+		elif live_cube == 5:
+			self.work()
 		self.final()
 	def final(self):
 		if self.progress == 100:
@@ -40,6 +49,23 @@ class Student:
 			print('Depression :(')
 			self.alive = False
 
+class University:
+	def __init__(self, title):
+		self.specialization = 'None'
+		self.address = 'None'
+		self.title = title
+		self.descriptopn = 'None'
+	def study(self, Student):
+		print(Student.name, 'is studying')
+	def pay_for_study(self, Mother):
+		print(Mother.name,'paid for study')
+
+class Mother:
+	def __init__(self, name):
+		self.name = name
+	def argue(self):
+		print("I'm angry!!!")
+		
 print('Bob\'s life')
 obj1 = Student('Bob')
 for i in range(365):
@@ -53,3 +79,11 @@ for i in range(365):
 	if obj2.alive == False:
 		break
 	obj2.live()
+
+univer = University('Oxford')
+univer.study(obj2)
+obj2.education(univer)
+
+mom = Mother('Chloe')
+mom.argue()
+univer.pay_for_study(mom)
